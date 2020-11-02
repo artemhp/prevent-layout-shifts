e3f8ec485824d8bddd9bdabcb07d03cc3cbb389f
Artem Deikun <artem.deikun@zorallabs.com>
2020-11-02 11:04:38 +0200

    Toggle basic

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.toggle').classList.add('hidden');
+document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelectorAll('.toggle').forEach(el => {
+        if(el.classList.contains('hidden')) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+    });
+})
